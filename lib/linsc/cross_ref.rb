345557c50858d3d601cdd2b7f8c40b5040793608
Dan Molloy <danieljmolloy1@gmail.com>
2016-03-17 14:56:48 +0000

    minor refactoring and renaming crossref variables in prep for major crossref generalizing refactor

--- lib/linsc/cross_ref.rb
+++ lib/linsc/cross_ref.rb
@@ -1,86 +1,78 @@
 require './linsc/csv_handlers'
-
+#sf -> master sheet
+#lin -> child sheet
 class CrossRef
   include CSVHandlers
 
-  def initialize(input_dir, lin, sf)
+  def initialize(input_dir, child_path, master_path)
     @input_dir = input_dir
-    @lin_input = lin
-    @sf_input = sf
-    @output_file = "#{@input_dir}crossref_fresh.csv"
-    @headers = get_headers(sf)
+    @child_path = child_path
+    @master_path = master_path
+    @output_file = "#{@input_dir}crossref.csv"
+    @headers = get_headers(@master_path)
     create_file(@output_file)
     cross_ref
   end
 
   def cross_ref
-    sf_data = CSV.read(@sf_input, headers: true)
-    sf_data = sf_data.sort do |x, y|
+    master_data = CSV.read(@master_path, headers: true)
+    master_data = master_data.sort do |x, y|
       a = x['Email']
       b = y['Email']
-      puts "{#{a} <=> #{b}}"
-      puts "{#{a.class} <=> #{b.class}}"
-      puts "#{a <=> b}"
       a && b ? a <=> b : a ? -1 : 1
     end
-    sf_data.first(10).each do |x| puts x['Email'] end
-    sf_emails = sf_data.collect {|row| row['Email']&.downcase}
+    master_lookup_values = master_data.collect {|row| row['Email']&.downcase}
     i = 0
-    q = 0
-    g = 0
-    CSV.foreach(@lin_input, headers: true, encoding: 'windows-1252') do |lin_row|
+    CSV.foreach(@child_path, headers: true, encoding: 'windows-1252') do |child_row|
       i += 1
-      puts "lin row: #{i}"
-      lin_email = lin_row['E-mail Address']&.downcase
-      if lin_email&.include?('@')
-        sf_match = sf_emails.bsearch_index do |sf_email|
-           lin_email && sf_email ? lin_email <=> sf_email : lin_email ? -1 : 1
+      puts "child row: #{i}"
+      child_lookup_value = child_row['E-mail Address']&.downcase
+      if child_lookup_value&.include?('@')
+        match_index = master_lookup_values.bsearch_index do |master_lookup_value|
+           child_lookup_value && master_lookup_value ?
+                child_lookup_value <=> master_lookup_value : child_lookup_value ? -1 : 1
         end
-        if !sf_match
-          sf_match = sf_data.find_index do |sf_row|
-            sf_row_emails = [sf_row['Email 2']&.downcase, sf_row['Email 3']&.downcase]
-            sf_row_emails.include?(lin_email)
+        if !match_index
+          match_index = master_data.find_index do |master_row|
+            master_secondary_lookups = [master_row['Email 2']&.downcase, sf_row['Email 3']&.downcase]
+            master_secondary_lookups.include?(child_lookup_value)
           end
-          q += 1 if sf_match
-        else
-          g += 1
         end
-        if sf_match
-          append_to_csv(@output_file, splice_rows(sf_data[sf_match], lin_row))
+        if match_index
+          append_to_csv(@output_file, splice_rows(master_data[match_index], child_row))
         else
-          append_to_csv(@output_file, convert_row(lin_row))
+          append_to_csv(@output_file, convert_row(child_row))
         end
       else
         puts "missing email"
       end
     end
-    puts "#{g} emails in field 1, #{q} in 2 or 3"
   end
 
-  def splice_rows(sf_row, lin_row)
-    sf_row['Candidate Source'] = lin_row['Recruiter']
-    sf_row['First Name'] = lin_row['First Name'] if lin_row['First Name']
-    sf_row['Last Name'] = lin_row['Last Name'] if lin_row['Last Name']
-    sf_row['Employer Organization Name 1'] = lin_row['Company'] if lin_row['Company']
-    sf_row['Employer 1 Title'] = lin_row['Job Title'] if lin_row['Job Title']
-    sf_row_ansi = CSV::Row.new(@headers, [])
-    sf_row.each do |key, value|
-      sf_row_ansi[key] = value&.encode('windows-1252', invalid: :replace, undef: :replace, replace: '#')
+  def splice_rows(master_row, child_row)
+    master_row['Candidate Source'] = child_row['Recruiter']
+    master_row['First Name'] = child_row['First Name'] if child_row['First Name']
+    master_row['Last Name'] = child_row['Last Name'] if child_row['Last Name']
+    master_row['Employer Organization Name 1'] = child_row['Company'] if child_row['Company']
+    master_row['Employer 1 Title'] = child_row['Job Title'] if child_row['Job Title']
+    master_row_new = CSV::Row.new(@headers, [])
+    master_row.each do |key, value|
+      sf_row_new[key] = value#&.encode('windows-1252', invalid: :replace, undef: :replace, replace: '#')
     end
-    sf_row_ansi
+    sf_row_new
 
   end
 
-  def convert_row(lin_row)
-    sf_row = CSV::Row.new(@headers, [])
-    sf_row['Candidate Source'] = lin_row['Recruiter']
-    sf_row['First Name'] = lin_row['First Name']
-    sf_row['Last Name'] = lin_row['Last Name']
-    sf_row['Email'] = lin_row['E-mail Address']
-    sf_row['Employer Organization Name 1'] = lin_row['Company']
-    sf_row['Employer 1 Title'] = lin_row['Job Title']
-    sf_row['Account Name'] = 'Candidates'.encode('windows-1252')
-    sf_row
+  def convert_row(child_row)
+    master_row = CSV::Row.new(@headers, [])
+    master_row['Candidate Source'] = child_row['Recruiter']
+    master_row['First Name'] = child_row['First Name']
+    master_row['Last Name'] = child_row['Last Name']
+    master_row['Email'] = child_row['E-mail Address']
+    master_row['Employer Organization Name 1'] = child_row['Company']
+    master_row['Employer 1 Title'] = child_row['Job Title']
+    master_row['Account Name'] = 'Candidates'#.encode('windows-1252')
+    master_row
   end
 
   def collect_emails(file)
@@ -90,9 +82,4 @@ class CrossRef
     end
     emails
   end
-
-  def get_headers(file)
-    CSV.open(file, headers: true, return_headers: true).shift.headers
-  end
-
 end

8bce1a78149b3d85c662de3955299bf1473a76b9
Dan Molloy <danieljmolloy1@gmail.com>
2016-03-20 20:47:07 +0000

    proxy handling

--- lib/linsc/proxy.rb
+++ lib/linsc/proxy.rb
@@ -1,5 +1,26 @@
 class Proxy
   attr_accessor :ip, :port, :username, :password, :status, :last_used
 
+  def initialize(ip:, port: 80, username: nil, password: nil, status: nil, last_used: nil)
+    @ip, @port, @username, @password, @status, @last_used =
+                  ip, port, username, password, status, last_used
+  end
 
+  def dead
+    @status = 'dead'
+    @last_used = Time.now
+  end
+
+  def good
+    @status = 'good'
+    @last_used = Time.now
+  end
+
+  def good?
+    @status == 'good' ? true : false
+  end
+
+  def dead?
+    @status == 'dead' ? true : false
+  end
 end
--- lib/linsc/proxy_handler.rb
+++ lib/linsc/proxy_handler.rb
@@ -1,3 +1,33 @@
+require_relative 'proxy'
+
 class ProxyHandler
 
+  def initialize(cooldown_time = 5)
+    @cooldown_time = cooldown_time
+    @proxy_list = File.read('./../data/proxies.txt').split("\n")
+    .collect{|proxy| proxy.split(':')}
+    @proxies = []
+
+    @proxy_list.each do |proxy_details|
+      proxy = Proxy.new(ip: proxy_details[0], port: proxy_details[1],
+      username: proxy_details[2], password: proxy_details[3], status: 'good',
+      last_used: Time.now - @cooldown_time)
+      @proxies << proxy
+    end
+
+  end
+
+  def get_proxy
+    @good_proxies = @proxies.select { |proxy| proxy.good? }
+    if @good_proxies.length > 0
+      @good_proxies.sort!{|a, b| a.last_used <=> b.last_used}
+      best_proxy = @good_proxies.first
+      duration = Time.now - best_proxy.last_used
+      puts duration
+      sleep(@cooldown_time - duration) if duration < @cooldown_time
+      best_proxy
+    else
+      false
+    end
+  end
 end
